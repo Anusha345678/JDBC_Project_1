39b225b95c9eccfa2e87e27c740911ddcb229a7f
Anusha345678 <anushasiddu29@gmail.com>
2025-05-06 09:31:53 +0530

    Update P1.java

--- src/com/Test1/P1.java
+++ src/com/Test1/P1.java
@@ -5,6 +5,10 @@ public class P1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello World");
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int n = s.nextInt();
+		System.out.println("Square of "+ n +" is: "+ (n*n));
 	}
 
 }
